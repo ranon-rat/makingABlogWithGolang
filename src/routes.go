7cdebfa5dca2937ee9878833d16bf60c20710517
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2021-01-05 12:25:42 -0600

    deleting dead code

--- src/routes.go
+++ src/routes.go
@@ -14,25 +14,22 @@ func routes() {
 	r := mux.NewRouter()
 	// REDIRIJE
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		number, err := getTheSizeOfTheQuery()
-		if err != nil || number <= 0 {
-			log.Println("something is wrong", err, number)
-			w.Write([]byte("something is wrong sorry"))
-			return
-		}
 
 		http.Redirect(w, r, "/1", 301)
 	})
+	// load the page
 	r.HandleFunc("/{page:[0-9]+}", func(w http.ResponseWriter, r *http.Request) {
 
 		http.ServeFile(w, r, "view/home.html")
 	})
+	// send the files
 	r.HandleFunc("/public/{directory+}", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, strings.ReplaceAll(r.URL.Path, "&", "/")[1:])
 	})
-
 	r.HandleFunc("/admin/postfile", newPost)
+	// get the info
 	r.HandleFunc("/api/{page:[0-9]+}", api)
+	// render the publication
 
 	r.HandleFunc("/publication/{id:[0-9]+}", renderInfo)
 
